08cad365c3a6585770cbced9b0671c6eea0cd382
Daniel Johansson <dannesnake@gmail.com>
2024-04-06 11:32:58 +0200

    Create DbContext class Create a class that inherit DbContext to be able to communicate with the database

---
+++ Labb1-LINQ/Data/SchoolDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Labb1_LINQ.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1_LINQ.Data
+{
+    internal class SchoolDbContext : DbContext
+    {
+        public DbSet<Course> Course {  get; set; }
+        public DbSet<Student> Student {  get; set; }
+        public DbSet<Subject> Subject {  get; set; }
+        public DbSet<Teacher> Teacher {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer("Data Source = LAPTOP-4AAOB4IN; Initial Catalog=Labb1-LINQDB; Integrated Security = True;");
+        }
+    }
+}
